f1bc9b2cdcc7cdf19d40aa550b787cd9aa181fe0
dojinss <ehwls20@gmail.com>
2024-09-11 16:23:14 +0900

    게시판 삭제

--- boardapp/src/main/java/com/aloha/DAO/BoardDAO.java
+++ boardapp/src/main/java/com/aloha/DAO/BoardDAO.java
@@ -1,37 +1,28 @@
 package com.aloha.DAO;
 
-import com.aloha.DTO.Board;
 import java.sql.SQLException;
 
-public class BoardDAO extends JDBC {
-
-    /***
-     * 데이터 수정
-     */
-    public int update(Board board){
+public class BoardDAO {
+    
+    /**
+	 * 게시글 삭제
+	 * @param no
+	 * @return
+	 */
+    public int delete(int no){
         int result = 0;
+        String sql = " DELETE FROM board "
+                   + " WHERE no = ? ";
         
-        String sql = " update board"
-						+"		set title = ?"
-						+ "		,writer = ?"
-						+ "		,content = ?"
-						+ "		,upd_date = now()"
-						+ "		,view=?"
-						+ "	where no = ?"; 
-
         try {
-            psmt = con.prepareStatement(sql);
-            psmt.setString(1, board.getTitle());
-            psmt.setString(2, board.getWriter());
-            psmt.setString(3, board.getContent());
-            psmt.setInt(4, board.getView());
-            psmt.setInt(5, board.getView());
-            result = psmt.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println("게시글 변경 시, 예외 발생");
-            e.printStackTrace();
-        }
-
-        return result;
+			psmt = con.prepareStatement(sql);			
+			psmt.setInt( 1, no);						
+			result = psmt.executeUpdate();				
+			
+		} catch (SQLException e) {
+			System.err.println("게시글 삭제 시, 예외 발생");
+			e.printStackTrace();
+		}
+		return result;
     }
 }
