c1d548bc79c9095d4bde4f40d2cba3b48fcb3018
wwwalohacampus <h850415@naver.com>
2024-09-11 17:51:54 +0900

    public 수정

--- boardapp/src/main/java/com/aloha/App.java
+++ boardapp/src/main/java/com/aloha/App.java
@@ -22,14 +22,23 @@ public class App extends Application {
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+    public static void setRoot(String fxml) throws Exception {
+		scene.setRoot(loadFXML(fxml));
+	}
+	
+	public static void setRoot(Parent root) throws Exception {
+		scene.setRoot(root);
+	}
+	
+	public static Parent loadFXML(String fxml) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+		return fxmlLoader.load();
+	}
+	
+	public static void exit() {
+		Stage stage = (Stage) scene.getWindow();
+		stage.close();
+	}
 
     public static void main(String[] args) {
         launch();
