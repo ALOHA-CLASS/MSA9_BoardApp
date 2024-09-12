6d081472cdc690906a4e1b8199d2eafb9b305168
OhSeungWon00 <ajtwodtmdnjs@gmail.com>
2024-09-12 10:48:14 +0900

    aaa

--- boardapp/src/main/java/com/aloha/DAO/BoardDAO.java
+++ boardapp/src/main/java/com/aloha/DAO/BoardDAO.java
@@ -121,7 +121,6 @@ public class BoardDAO extends JDBC {
 
         return result;
     }
-<<<<<<< HEAD
 
     /*
      * 게시글 삭제
@@ -142,30 +141,3 @@ public class BoardDAO extends JDBC {
         return result;
     }
 }
-=======
-}
-=======
-    public Board select(int no) {
-		Board board = new Board();
-		String sql = " SELECT * "
-				   + " FROM board "
-				   + " WHERE no = ? ";
-		try {
-			psmt = con.prepareStatement(sql);
-			psmt.setInt( 1, no );		
-			rs = psmt.executeQuery();
-			
-			if( rs.next() ) {
-				board.setTitle( rs.getString("title") );
-				board.setWriter( rs.getString("writer") );
-				board.setContent( rs.getString("content") );
-			}	
-		} catch (SQLException e) {
-			System.err.println("게시글 조회 오류");
-			e.printStackTrace();
-		}
-		return board;
-    }
-}
->>>>>>> 363a1eb (게시글 조회)
->>>>>>> a32d470 (게시글 조회)
